86b6857e363b0b866b85f5cca5e14f7c26d26cab
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2020-11-10 16:15:33 +0100

    added if instruction

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const app = () => {
-    const song = document.querySelector('.song');
-    const play = document.querySelector('.play');
-    const outline = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('.vid-container video');
+    const song = document.querySelector(".song");
+    const play = document.querySelector(".play");
+    const outline = document.querySelector(".moving-outline circle");
+    const video = document.querySelector(".vid-container video");
 
     //Sounds
-    const sounds = document.querySelector('.sound-picker button');
+    const sounds = document.querySelector(".sound-picker button");
     //Time Display
-    const timeDisplay = document.querySelector('.time-display');
+    const timeDisplay = document.querySelector(".time-display");
     //Get the Length of the outline
     const outlineLength = outline.getTotalLength();
     //Duration
@@ -17,10 +17,23 @@ const app = () => {
     outline.style.strokeDashoffset = outlineLength;
 
     //play sound
-    play.addEventListener('clikc', () =>{
-        song.play();
+    play.addEventListener('click', () =>{
+        checkPlaying(song);
     });
-}
+
+    //create a function specific to stop and play the sounds
+    const checkPlaying = song => {
+        if(song.paused) {
+            song.play();
+            video.play();
+            play.src ="./svg/pause.svg";
+        }else{
+            song.pause();
+            video.pause();
+            play.src = "./svg/play.svg";
+        }
+    };
+};
 
 
 app();
